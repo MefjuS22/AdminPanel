755ad399e83962f742ae2431a689aae46f0a674a
MefjuS22 <smydamateusz9@gmail.com>
2022-11-14 10:47:15 +0100

    primevue example"

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { ref } from "vue";
+import { RouterLink, RouterView } from "vue-router";
+const text = ref();
+</script>
+
+<template>
+  <div class="container">
+    <span class="p-float-label">
+      <InputText id="txt" v-model="text" />
+      <label for="txt">Text</label>
+    </span>
+    <Button label="Greet" class="p-button-raised" icon="pi pi-user" />
+  </div>
+  <RouterView />
+</template>
+
+<style lang="scss">
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 100vh;
+}
+</style>
